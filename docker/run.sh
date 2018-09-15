bc43de65b0351b2b37e5d7f6f8da8b7b64ccaf39
UthpalaPitawela <uthpala.isiru@gmail.com>
2018-09-15 10:15:58 +0530

    adding gitignore and run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -3,4 +3,4 @@
 PORT=88
 IMAGE_NAME=pubuduwelagedara/blocksci
 
-docker run -it -v /home/uthpala/Uthpala/Uthpala/blocksci/blocksci-data:/root/bitcoin-data -v ./../src:/root/BlockSci/src -p $PORT:8888 $IMAGE_NAME
+docker run -it -v /home/uthpala/Uthpala/Uthpala/blocksci/blocksci-data:/root/bitcoin-data -v src:/root/BlockSci/src/heuristics -p $PORT:8888 $IMAGE_NAME
