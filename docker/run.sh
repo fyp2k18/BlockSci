9f394e0f9aceb61dd183df96e68bfa2b034a873a
UthpalaPitawela <uthpala.isiru@gmail.com>
2018-09-15 21:17:02 +0530

    changed the code according to the server setup

--- docker/run.sh
+++ docker/run.sh
@@ -3,4 +3,5 @@
 PORT=88
 IMAGE_NAME=pubuduwelagedara/blocksci
 
-docker run -it -v /home/uthpala/Uthpala/Uthpala/blocksci/blocksci-data:/root/bitcoin-data -v src:/root/BlockSci/src/heuristics -p $PORT:8888 $IMAGE_NAME
+#sudo docker run -it -d --name fypBlockSci -v /home/uthpala/Uthpala/Uthpala/blocksci/blocksci-data:/root/bitcoin-data -v /home/uthpala/Uthpala/research/BlockSci/src:/root/BlockSci/src -p $PORT:8888 $IMAGE_NAME
+sudo docker run -it -d --name fypBlockSci -v /home/wasn/bitcoin-data/blocksci-514496:/root/bitcoin-data -v /home/wasn/BlockSci/src:/root/BlockSci/src -p $PORT:8080 $IMAGE_NAME
