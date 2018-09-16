c6e7612ddebcc974a24a89a06e724ab90a2a5c64
UthpalaPitawela <uthpala.isiru@gmail.com>
2018-09-16 10:07:44 +0530

    updated the run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-PORT=88
-IMAGE_NAME=pubuduwelagedara/blocksci
+PORT=90
+IMAGE_NAME=pubuduwelagedara/blockscitemp
+NAME=fypBlockScitemp
 
-#sudo docker run -it -d --name fypBlockSci -v /home/uthpala/Uthpala/Uthpala/blocksci/blocksci-data:/root/bitcoin-data -v /home/uthpala/Uthpala/research/BlockSci/src:/root/BlockSci/src -p $PORT:8888 $IMAGE_NAME
-sudo docker run -it -d --name fypBlockSci -v /home/wasn/bitcoin-data/blocksci-514496:/root/bitcoin-data -v /home/wasn/BlockSci/src:/root/BlockSci/src -p $PORT:8080 $IMAGE_NAME
+sudo docker run -it -d --name $NAME -v /home/uthpala/Uthpala/Uthpala/blocksci/blocksci-data:/root/bitcoin-data -v /home/uthpala/Uthpala/research/BlockSci/src:/root/BlockSci/src -p $PORT:8888 $IMAGE_NAME
+
